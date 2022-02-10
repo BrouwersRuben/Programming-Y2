8f0db77c64a29f3a584580394592254475bcd7a8
rubenbrouwers <KdGD89BbGw>
2022-02-11 00:30:28 +0100

    changed http code

--- Project/src/main/java/be/kdg/java2/project/exceptions/LocationNotFoundException.java
+++ Project/src/main/java/be/kdg/java2/project/exceptions/LocationNotFoundException.java
@@ -3,7 +3,7 @@ package be.kdg.java2.project.exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(value= HttpStatus.NOT_FOUND, reason="No such Location" )
+@ResponseStatus(value= HttpStatus.NO_CONTENT, reason="No such Location" )
 public class LocationNotFoundException extends RuntimeException{ //<-- A Runtime exception
     private String location;
     public LocationNotFoundException(String message, String location) {
