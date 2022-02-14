579c35c05f8395fc6fcca61ebc1852df15ccbfd4
rubenbrouwers <KdGD89BbGw>
2022-02-14 15:38:22 +0100

    added modelmapper

--- Project/src/main/java/be/kdg/java2/project/configuration/WebConfiguration.java
+++ Project/src/main/java/be/kdg/java2/project/configuration/WebConfiguration.java
@@ -1,6 +1,8 @@
 package be.kdg.java2.project.configuration;
 
 import be.kdg.java2.project.converters.StringToBuildingTypeConverter;
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -10,5 +12,10 @@ public class WebConfiguration implements WebMvcConfigurer {
     @Override
     public void addFormatters(FormatterRegistry registry) {
         registry.addConverter(new StringToBuildingTypeConverter());
+    }
+
+    @Bean
+    public ModelMapper modelMapper(){
+        return new ModelMapper();
     }
 }
