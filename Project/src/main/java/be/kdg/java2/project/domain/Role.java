0e81907e8f4c83a41347d46c35eb1dbd1e2d32df
rubenbrouwers <KdGD89BbGw>
2022-03-14 18:26:52 +0100

    adding the user role, and some other additional stuff

--- Project/src/main/java/be/kdg/java2/project/domain/Role.java
+++ Project/src/main/java/be/kdg/java2/project/domain/Role.java
@@ -1,7 +1,15 @@
 package be.kdg.java2.project.domain;
 
 public enum Role {
-    U, CD
-    //U = Update
-    //CD = Create and Delete
+    N("ROLE_USER"), U("ROLE_UPDATER"), CD("ROLE_CREATOR");
+
+    private final String name;
+
+    Role(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
