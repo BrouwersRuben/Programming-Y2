0860ab8a0b7f84c881fd92bc66219a771a2de502
rubenbrouwers <KdGD89BbGw>
2022-03-14 10:53:09 +0100

    csrf stuff, far from finished

--- Project/src/main/java/be/kdg/java2/project/security/WebSecurityConfig.java
+++ Project/src/main/java/be/kdg/java2/project/security/WebSecurityConfig.java
@@ -10,6 +10,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 @Configuration
 @EnableWebSecurity
@@ -34,7 +35,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 //TODO: fix this
                 .csrf()
-                    .disable()
+                    .csrfTokenRepository(
+                            CookieCsrfTokenRepository.withHttpOnlyFalse())
+                .and()
             .formLogin()
                 .loginPage("/login")
                     .permitAll()
