d8e63960bd02ccf6177c6692e1f12212a31453c1
rubenbrouwers <KdGD89BbGw>
2022-03-14 18:26:24 +0100

    making 2 annotations for the roles

---
+++ Project/src/main/java/be/kdg/java2/project/security/UpdaterOnly.java
@@ -0,0 +1,14 @@
+package be.kdg.java2.project.security;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+@PreAuthorize("hasRole('ROLE_UPDATER')")
+public @interface UpdaterOnly {
+}
