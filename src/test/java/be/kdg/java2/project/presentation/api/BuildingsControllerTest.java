68cc0cc40f917307041fafbdf1ab224cbe21ec14
rubenbrouwers <KdGD89BbGw>
2022-03-23 21:37:54 +0100

    added dirtiesContext

--- src/test/java/be/kdg/java2/project/presentation/api/BuildingsControllerTest.java
+++ src/test/java/be/kdg/java2/project/presentation/api/BuildingsControllerTest.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -20,6 +21,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @ActiveProfiles("test")
 @SpringBootTest
 @AutoConfigureMockMvc
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 class BuildingsControllerTest {
 
     // Commented out @CreaterOnly on postmapping REST API
