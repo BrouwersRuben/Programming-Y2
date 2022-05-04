ec4b92c66e5ab7e3659a0c16d2d990b57db5e7be
Ruben Brouwers <ruben.brouwers@student.kdg.be>
2022-05-04 09:56:29 +0200

    added a verify to put architect

--- src/test/java/be/kdg/java2/project/presentation/api/ArchitectControllerMockingTests.java
+++ src/test/java/be/kdg/java2/project/presentation/api/ArchitectControllerMockingTests.java
@@ -9,6 +9,7 @@ import be.kdg.java2.project.services.ArchitectService;
 import be.kdg.java2.project.services.UserService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -20,7 +21,9 @@ import org.springframework.test.web.servlet.MockMvc;
 import java.time.LocalDate;
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
@@ -72,6 +75,13 @@ public class ArchitectControllerMockingTests {
                         .with(user(customUserDetails))
                         .content(objectMapper.writeValueAsString(update))
         ).andExpect(status().isNoContent());
+
+        ArgumentCaptor<Architect> captor = ArgumentCaptor.forClass(Architect.class);
+        verify(architectService).updateArchitect(captor.capture());
+
+        var capturedArchitect = captor.getValue();
+        assertEquals(update.getId(), capturedArchitect.getId());
+        assertEquals(update.getNumberOfEmployees(), capturedArchitect.getNumberOfEmployees());
     }
 
 
